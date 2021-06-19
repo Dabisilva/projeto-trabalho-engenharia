5e0cbe7c4856275af197c946bb055bc8f2f1385b
Gabriel Ilha Guerra de Paiva <ilhaguerra@gmail.com>
2021-06-19 18:45:54 -0300

    redução de linhas de código, correção do updateLevelStats e updateUser, criação do updatePassword e leaderBoard

--- 03_Analise_e_projeto/projeto/BackEnd/app.py
+++ 03_Analise_e_projeto/projeto/BackEnd/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, jsonify, abort
 from util import bd
 from flask_cors import CORS
+import bcrypt
 
 app = Flask(__name__)
 
 CORS(app)
+
+mysql = bd.SQL('root', 'root', 'moveit')
 
 @app.route('/create', methods = ['POST'])
 def create():
@@ -14,17 +17,21 @@ def create():
         nome = content['nome']
         email = content['email']
         senha = content['senha']
+        encrypted = senha.encode("utf-8")
+        hashed = bcrypt.hashpw(encrypted, bcrypt.gensalt())
 
-        mysql = bd.SQL("root","root", "moveit")
         comando = "SELECT * FROM user WHERE email = %s"
         cs = mysql.consultar(comando, [email])
         dados = cs.fetchone()
 
         if dados == None:
             comando1 = "INSERT INTO user(nome, email, senha, xp, challenges, level) VALUES (%s, %s, %s, 0, 0, 1)"
-            mysql.executar(comando1, [nome, email, senha])
+            mysql.executar(comando1, [nome, email, hashed])
 
-            user = {"nome": dados[1], "email": dados[2], "xp": dados[4], "challenges": dados[5], "level": dados[6]}
+            comando = "SELECT * FROM user WHERE email = %s"
+            cs = mysql.consultar(comando, [email])
+            dados = cs.fetchone()
+            user = {"id": dados[0], "nome": dados[1], "email": dados[2], "xp": dados[4], "challenges": dados[5], "level": dados[6]}
             return jsonify(user)
 
         elif dados:
@@ -37,23 +44,29 @@ def login():
 
         type = content['type']
         nome = content['nome']
-        mysql = bd.SQL('root','root', 'moveit')
+        email = content['email']
 
         if type == 'normal':
 
             senha = content['senha']
-
-            comando = "SELECT * FROM user WHERE nome = %s AND senha = %s;"
-            cs = mysql.consultar(comando, [nome, senha])
+            encrypted = senha.encode("utf-8")
+            comando = "SELECT * FROM user WHERE email = %s;"
+            cs = mysql.consultar(comando, [email])
             dados = cs.fetchone()
-
             if dados:
-                user = {"nome": dados[1], "email": dados[2], "xp": dados[4], "challenges": dados[5], "level": dados[6]}
-                return jsonify(user)
+                passwordHash = dados[3]
+                if bcrypt.checkpw(encrypted, passwordHash.encode("utf-8")):
+                    if dados:
+                        user = {"id": dados[0], "nome": dados[1], "email": dados[2], "xp": dados[4],
+                                "challenges": dados[5], "level": dados[6]}
+                        return jsonify(user)
+                    else:
+                        return jsonify(message="erro ao tentar logar"), 401
+                else:
+                    return jsonify(message="Senha invalida"), 401
             else:
-                return jsonify(message="erro ao tentar logar"), 401
+                return jsonify(message="Email não cadastrado"), 401
         else:
-            email = content['email']
             comando = "SELECT * FROM user WHERE email = %s;"
             cs = mysql.consultar(comando, [email])
             dados = cs.fetchone()
@@ -65,58 +78,56 @@ def login():
                 comando = "SELECT * FROM user WHERE email = %s"
                 cs = mysql.consultar(comando, [email])
                 dados = cs.fetchone()
-                user = {"nome": dados[1], "email": dados[2], "xp": dados[4], "challenges": dados[5], "level": dados[6]}
+                user = {"id": dados[0], "nome": dados[1], "email": dados[2], "xp": dados[4], "challenges": dados[5], "level": dados[6]}
                 return jsonify(user)
 
             elif dados != None:
-                user = {"nome": dados[1], "email": dados[2], "xp": dados[4], "challenges": dados[5], "level": dados[6]}
+                user = {"id": dados[0], "nome": dados[1], "email": dados[2], "xp": dados[4], "challenges": dados[5], "level": dados[6]}
                 return jsonify(user)
 
             else:
                 return jsonify(message="erro ao tentar logar"), 401
 
-@app.route('/updateLevelStats', methods = ['PUT'])
+@app.route('/updateLevelStats', methods = ['POST'])
 def updateLevelStats():
 
     content = request.get_json()
-    email = content['email']
+    id = content['id']
     xp = content['xp']
     challenges = content['challenges']
     level = content['level']
 
-    mysql = bd.SQL("root", "root", "moveit")
-    comando = "UPDATE user SET xp = %s, challenges = %s, level = %s WHERE email = %s"
-    update = mysql.executar(comando,[xp, challenges, level, email])
+    comando = "UPDATE user SET xp = %s, challenges = %s, level = %s WHERE id = %s"
+    update = mysql.executar(comando,[xp, challenges, level, id])
 
     if update:
-        comando = "SELECT * FROM user WHERE email = %s"
-        cs = mysql.consultar(comando, [email])
+        comando = "SELECT * FROM user WHERE id = %s;"
+        cs = mysql.consultar(comando, [id])
         dados = cs.fetchone()
-        user = {"nome": dados[1], "email": dados[2], "xp": dados[4], "challenges": dados[5], "level": dados[6]}
+        user = {"id": dados[0], "nome": dados[1], "email": dados[2], "xp": dados[4],
+                "challenges": dados[5], "level": dados[6]}
         return jsonify(user)
 
     else:
         return jsonify(message = "erro ao dar update"), 401
 
-@app.route('/updateUser', methods = ['PUT'])
+@app.route('/updateUser', methods = ['POST'])
 def updateUser():
 
     content = request.get_json()
+    id = content['id']
     nome = content['nome']
     email = content['email']
     newEmail = content['newEmail']
-    senha = content['senha']
-
-    mysql = bd.SQL("root", "root", "moveit")
 
     if email == newEmail:
 
-        comando = "UPDATE user SET nome = %s, email = %s, senha = %s WHERE email = %s"
-        update = mysql.executar(comando, [nome, email, senha, email])
+        comando = "UPDATE user SET nome = %s, email = %s WHERE id = %s"
+        update = mysql.executar(comando, [nome, email, id])
 
         if update:
-            comando = "SELECT * FROM user WHERE email = %s"
-            cs = mysql.consultar(comando, [email])
+            comando = "SELECT * FROM user WHERE id = %s"
+            cs = mysql.consultar(comando, [id])
             dados = cs.fetchone()
             user = {"nome": dados[1], "email": dados[2], "xp": dados[4], "challenges": dados[5], "level": dados[6]}
             return jsonify(user)
@@ -131,14 +142,14 @@ def updateUser():
         data = check.fetchone()
         if data == None:
 
-            comando = "UPDATE user SET nome = %s, email = %s, senha = %s WHERE email = %s"
-            update = mysql.executar(comando, [nome, newEmail, senha, email])
+            comando = "UPDATE user SET nome = %s, email = %s WHERE id = %s"
+            update = mysql.executar(comando, [nome, newEmail, id])
 
             if update:
-                comando = "SELECT * FROM user WHERE email = %s"
-                cs = mysql.consultar(comando, [newEmail])
+                comando = "SELECT * FROM user WHERE id = %s"
+                cs = mysql.consultar(comando, [id])
                 dados = cs.fetchone()
-                user = {"nome": dados[1], "email": dados[2], "xp": dados[4], "challenges": dados[5], "level": dados[6]}
+                user = {"id": dados[0], "nome": dados[1], "email": dados[2], "xp": dados[4], "challenges": dados[5], "level": dados[6]}
                 return jsonify(user)
 
             else:
@@ -147,5 +158,39 @@ def updateUser():
         if data:
             return jsonify(message="Erro ao dar update: email já cadastrado no sistema"), 401
 
+@app.route('/updatePassword', methods = ['PUT'])
+def updatePassword():
+    content = request.get_json()
+    novaSenha = content['novaSenha']
+    id = content['id']
+
+    encrypted = novaSenha.encode("utf-8")
+    comando = "SELECT senha FROM user WHERE id = %s;"
+    cs = mysql.consultar(comando, [id])
+    dados = cs.fetchone()
+    passwordHash = dados[0]
+    if bcrypt.checkpw(encrypted, passwordHash.encode("utf-8")):
+        return jsonify(message='Favor não inserir senha salva no sistema'),401
+    else:
+        hashed = bcrypt.hashpw(encrypted, bcrypt.gensalt())
+        comando = "UPDATE user SET senha= %s WHERE id = %s"
+        update = mysql.executar(comando, [hashed,id])
+        if update:
+            return jsonify(message='Senha alterada com sucesso!'),200
+        else:
+            return jsonify(message='Erro na alteração de senha'),401
+
+
+@app.route('/leaderBoard', methods = ['GET'])
+def leaderBoard():
+    if request.method == 'GET':
+        comando = "SELECT * FROM user ORDER BY xp DESC"
+        cs = mysql.consultar(comando,[])
+        user = []
+        for [id, nome, email, senha, xp, challenges, level] in cs:
+            user.append({"id": id, "nome": nome, "email": email, "xp": xp, "challenges": challenges, "level": level})
+        cs.close()
+
+        return jsonify(user)
 
 app.run()
