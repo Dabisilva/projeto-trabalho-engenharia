19f02d18ae69e1e219598dd9daea1e91f534be8d
Gabriel Ilha Guerra de Paiva <ilhaguerra@gmail.com>
2021-06-21 22:02:44 -0300

    rotas e tabelas -- typing , challenges

--- 03_Analise_e_projeto/projeto/BackEnd/db/createTables.py
+++ 03_Analise_e_projeto/projeto/BackEnd/db/createTables.py
@@ -1,4 +1,4 @@
-from BackEnd.util import bd
+from util import bd
 
 mysql = bd.SQL("root", "root", "moveit")
 
@@ -7,17 +7,48 @@ comando = "DROP TABLE IF EXISTS user;"
 if mysql.executar(comando, ()):
    print ("Tabela de user excluída com sucesso!")
 
+comando = "DROP TABLE IF EXISTS typing;"
+
+if mysql.executar(comando, ()):
+   print ("Tabela de user excluída com sucesso!")
+
+comando = "DROP TABLE IF EXISTS challenges;"
+
+if mysql.executar(comando, ()):
+   print ("Tabela de user excluída com sucesso!")
 
 comando = '''
     CREATE TABLE user(
       id INT AUTO_INCREMENT PRIMARY KEY,
-      nome VARCHAR(50),
-      email VARCHAR(50),
-      senha VARCHAR(50),
-      xp INT,
-      challenges INT,
-      level INT);
+      nome VARCHAR(50) NOT NULL,
+      email VARCHAR(50) NOT NULL,
+      senha VARCHAR(50) NOT NULL,
+      xp INT NOT NULL,
+      challenges INT NOT NULL,
+      level INT NOT NULL);
 '''
 
 if mysql.executar(comando, ()):
    print ("Tabela de user criada com sucesso!")
+
+comando = '''
+    CREATE TABLE typing(
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      description TEXT NOT NULL,
+      amount INT NOT NULL);
+'''
+
+if mysql.executar(comando, ()):
+   print ("Tabela de typing criada com sucesso!")
+
+
+comando = '''
+    CREATE TABLE challenges(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        type VARCHAR(50) NOT NULL,
+        description TEXT NOT NULL,
+        amount INT NOT NULL);
+'''
+
+if mysql.executar(comando, ()):
+   print ("Tabela challenges criada com sucesso!")
